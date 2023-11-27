b50e5828a25fee3991768da49baec0ff2817dc47
Rocky9463 <rohit94639463@gmail.com>
2023-11-27 14:13:12 +0530

    Adding binary search in searching.py

--- Python/Searching.py
+++ Python/Searching.py
@@ -9,6 +9,25 @@ def linearSearch(arr, element):
             i+=1
     print("Element not found")
 
+def binarySearch(arr, element):
+    h = len(arr)-1
+    l = 0
+    while(l<=h):
+        m = (l+h)//2 
+        if arr[m] == element:
+            print("Element found at position : ", m+1)
+            return
+        else:
+            if element > arr[m]:
+                l = m+1
+            else:
+                h = m-1
+    print("Element not found")
+
+
 arr = [2,9,9,7,9,2,4,5,8]
 element = int(input("Enter element to search : "))
-linearSearch(arr, element)
+#linearSearch(arr, element)
+
+arr.sort()
+binarySearch(arr, element)

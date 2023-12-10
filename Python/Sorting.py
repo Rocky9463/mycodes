5396915a28f04d6fc56e916b1dd703f916eb714c
Rocky9463 <rohit94639463@gmail.com>
2023-12-10 15:24:12 +0530

    Added bubble and insertion sort

--- Python/Sorting.py
+++ Python/Sorting.py
@@ -5,6 +5,34 @@ def selectionSort(arr):
                 arr[i], arr[j] = arr[j], arr[i]
     print("After sorting : ", arr)
 
+def bubbleSort(arr):
+    i = len(arr)-1
+    while i >= 1:
+        j=0
+        while j<i:
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            j+=1
+        i-=1
+    print("After sorting : ", arr)
+
+def insertionSort(arr):
+    i=1
+    while(i<len(arr)):
+        x=arr[i]
+        j=i-1
+        while(j>=0):
+            if arr[j]>x:
+                arr[j+1] = arr[j]
+            else:
+                break
+            j-=1
+        arr[j+1] = x
+        i+=1
+    print("After sorting : ", arr)
+    
+
 arr = [2,9,9,7,9,2,4,5,8]
+#arr = [20,15,6,18,2]
 print("Before sorting : ", arr)
-selectionSort(arr)
+shellSort(arr)

e4aec9330e3429f98f75b40acd5687d092a99743
rohitk-9 <rohit94639463@gmail.com>
2024-03-15 17:20:36 +0530

    Adding labelled break and continue

---
+++ Java/LabeledBreakContinue.java
@@ -0,0 +1,28 @@
+public class LabeledBreakContinue {
+    public static void main(String[] args) {
+        // Labeled break example
+        outerLoop:
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Outer loop iteration: " + i);
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) {
+                    System.out.println("Breaking from outer loop");
+                    break outerLoop; // Breaks out of the outer loop
+                }
+                System.out.println("Inner loop iteration: " + j);
+            }
+        }
+
+        // Labeled continue example
+        firstLoop:
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (i == 1 && j == 1) {
+                    System.out.println("Continuing to next iteration of outer loop");
+                    continue firstLoop; // Continues with the next iteration of the outer loop
+                }
+                System.out.println("i: " + i + ", j: " + j);
+            }
+        }
+    }
+}

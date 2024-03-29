214590a463d4f6342d638b8bcfabaca1a645aeab
rohitk-9 <rohit94639463@gmail.com>
2024-03-29 12:30:02 +0530

    Changed folder names

---
+++ Java s2/tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+public void showCandidateDetails() {
+    //     for (Candidate candidate : candidate) {
+    //         System.out.println("Roll no. : " + candidate.name + " \nEnrollment : " + candidate.dob);
+    //         System.out.println("DOB : " + candidate.fatherName + "\n");
+    //     }
+    // }

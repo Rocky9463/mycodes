a49cf5cb4694ce9c8e86e1bd0fb88457282163a8
rohitk-9 <rohit94639463@gmail.com>
2024-10-03 21:28:29 +0530

    Inverse of matrix

--- IS_s3/Mat.java
+++ IS_s3/Mat.java
@@ -42,6 +42,82 @@ public class Mat {
         }   
         return result;
     }
+    public int determinant(int[][] mat) {
+        int n = mat.length;
+        
+        if (n == 1) {
+            return mat[0][0];
+        } else if (n == 2) {
+            return mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0];
+        }
+
+        int det = 0;
+        for (int i = 0; i < n; i++) {
+            int[][] subMatrix = cofactor(mat, 0, i);
+            int sign = (i % 2 == 0) ? 1 : -1;
+            det += sign * mat[0][i] * determinant(subMatrix);
+        }
+        return det;
+    }
+
+    public int[][] adjoint(int[][] mat){
+        int n = mat.length;
+        int[][] res = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j=0; j<n;j++){
+                int[][] subMatrix = cofactor(mat, i, j);
+                res[i][j] = determinant(subMatrix);
+                if ((i+1+j+1)%2 == 1){
+                    res[i][j] = -res[i][j];
+                }
+            }
+        }
+        return transposeMatrix(res);
+    }
+
+    public static int[][] transposeMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transposedMatrix = new int[cols][rows];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposedMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return transposedMatrix;
+    }
+
+    public double[][] invert(int[][] mat) {
+        int n = mat.length;
+        double[][] inv = new double[n][n];
+
+        int det = determinant(mat);
+        if (det == 0) {
+            System.out.println("Matrix is singular and cannot be inverted.");
+            return null;
+        }
+
+        double detDouble = det;
+        int[][] adjoint = adjoint(mat);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                inv[i][j] = adjoint[i][j]/detDouble;
+            }
+        }
+
+        return inv;
+    }
+
+
+
+    void display(double[][] a){
+        for(int i=0; i<a.length; i++){
+            for(int j=0; j<a[0].length; j++){
+                System.out.print(a[i][j]+"  ");
+            }
+            System.out.println();
+        }
+    }
     void display(int[][] a){
         for(int i=0; i<a.length; i++){
             for(int j=0; j<a[0].length; j++){
@@ -51,8 +127,17 @@ public class Mat {
         }
     }
     public static void main(String s[]){
-        Mat obj = new Mat(3);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        Mat obj = new Mat(n);
         obj.readElement();
-        obj.display(obj.cofactor(a, 0, 0));        
+        System.out.println("\nDeterminant : "+obj.determinant(a)); 
+        System.out.println("\nAdjoint : ");
+        obj.display(obj.adjoint(a));
+        System.out.println("\nInverse : ");
+        obj.display(obj.invert(a));
+
+        sc.close();
     }
 }
